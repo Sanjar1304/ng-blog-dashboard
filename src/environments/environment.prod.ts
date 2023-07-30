0e3944f6008da4ef737e012449c4826d9b17c413
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-07-30 17:28:40 +0500

    Firebase config linked to environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAU4qC1th1tUQL9VnafBGoAVqMX3kbrG98",
+    authDomain: "ng-blog-22d62.firebaseapp.com",
+    projectId: "ng-blog-22d62",
+    storageBucket: "ng-blog-22d62.appspot.com",
+    messagingSenderId: "418369442574",
+    appId: "1:418369442574:web:4f76cb6fe26622822c7d5f"
+  }
 };
