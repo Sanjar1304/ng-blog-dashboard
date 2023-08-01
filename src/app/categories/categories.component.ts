6c63fd8b1c9bcbf7bc9259556fca646673478dcf
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-08-01 21:03:45 +0500

    Added subcategory collections to firebase store

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -17,11 +17,26 @@ export class CategoriesComponent implements OnInit {
   onSubmit(formData: any){
 
    let categoryData = {
-    category: formData.value
+    category: formData.value.category,
+    status: 'active'
    }
+
+   let subCategoryData = {
+    subCategory: 'SubCategory1'
+   }
+
 
    this.fireStore.collection('categories').add(categoryData).then(docRef => {
     console.log(docRef);
+
+    this.fireStore.collection('categories').doc(docRef.id).collection('subcategories').add(subCategoryData).then(docRef1 => {
+      console.log(docRef1);
+
+      this.fireStore.collection('categories').doc(docRef.id).collection('subcategories').doc(docRef1.id).collection('subsubcategory').add(subCategoryData).then(docRef2 => {
+        console.log('Second Level SubCategory Saved Successfully');
+      })
+    })
+
    })
    .catch((error) => {
       console.log(error)
