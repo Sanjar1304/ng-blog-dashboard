546067ff83f1d1903f4dfbb0fd56dc55112b1b99
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-08-02 21:14:45 +0500

    Upgraded firestore data collection method

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -26,11 +26,8 @@ export class CategoriesComponent implements OnInit {
     }
 
     this.fireStore.collection('categories').add(categoryData).then(docRef => {
-      console.log(docRef);
       this.fireStore.doc(`categories/${docRef.id}`).collection('subcategories').add(subCategoryData).then(docRef1 => {
-        console.log(docRef1);
         this.fireStore.doc(`categories/${docRef.id}/subcategories/${docRef1.id}`).collection('subsubcategory').add(subCategoryData).then(docRef2 => {
-          console.log(docRef2);
           console.log('Second Level SubCategory Saved Successfully');
         })
       })
